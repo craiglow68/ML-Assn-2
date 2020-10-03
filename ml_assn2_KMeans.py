c4c41c9ef617160d17359eba78b94282dbc9455e
craiglow68 <43859307+craiglow68@users.noreply.github.com>
2020-10-03 03:09:23 -0400

    Update ml_assn2_KMeans.py

--- ml_assn2_KMeans.py
+++ ml_assn2_KMeans.py
@@ -1,21 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-pip install sklearn
-
-
-# In[3]:
-
-
-pip install yellowbrick
-
-
-# In[78]:
-
-
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
 from yellowbrick.cluster import KElbowVisualizer
@@ -52,16 +37,3 @@ print() #Formatting
 
 print("Confusion Matrix")
 print(confusion_matrix(y_true, ymeans))
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
